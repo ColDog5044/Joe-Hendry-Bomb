08068483c6dedb4204add149a01ce7661f530741
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2024-07-17 09:38:59 -0400

    Start building project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JoeHendryBomb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say his name and he appears...");
+            Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("I believe in Joe Hendry!");
+                }
+            }
+        }
+    }
+}
